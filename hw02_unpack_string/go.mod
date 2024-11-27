10c337a2ba9a7051604694bbb8ea325e42fdcbfc
frobbery <femalerobbery68@gmail.com>
2024-11-27 15:14:14 +0300

    HW2 is completed

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/frobbery/go_course/hw02_unpack_string
 
 go 1.22
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,39 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+
+	runes := []rune(s)
+
+	newRunes := make([]rune, 0, len(runes))
+
+	var prevRune rune
+	var prevRuneNum bool = false
+	for runeInd, currRune := range runes {
+		if repeatNum, err := strconv.ParseInt(string(currRune), 10, 32); err == nil {
+			if prevRuneNum || prevRune == 0 {
+				return "", ErrInvalidString
+			}
+			prevRuneNum = true
+			for i := int64(0); i < repeatNum; i++ {
+				newRunes = append(newRunes, prevRune)
+			}
+		} else {
+			if !prevRuneNum && prevRune != 0  {
+				newRunes = append(newRunes, prevRune)
+			}
+			if runeInd == len(runes) - 1 {
+				newRunes = append(newRunes, currRune)
+			}
+			prevRuneNum = false
+		}
+		prevRune = currRune
+	}
+	
+	return string(newRunes), nil
 }
