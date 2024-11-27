579487b8710dba1ea74bb010d3edd15a5d75499e
frobbery <femalerobbery68@gmail.com>
2024-11-27 16:34:50 +0300

    Add shielding

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,31 +9,39 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
 	runes := []rune(s)
-
 	newRunes := make([]rune, 0, len(runes))
-
 	var prevRune rune
-	var prevRuneNum bool = false
+	prevRuneNum := false
+	prevRuneShielded := false
 	for runeInd, currRune := range runes {
-		if repeatNum, err := strconv.ParseInt(string(currRune), 10, 32); err == nil {
-			if prevRuneNum || prevRune == 0 {
-				return "", ErrInvalidString
-			}
-			prevRuneNum = true
-			for i := int64(0); i < repeatNum; i++ {
-				newRunes = append(newRunes, prevRune)
-			}
+		if currRune == '\\' && !prevRuneShielded {
+			prevRuneShielded = true
 		} else {
-			if !prevRuneNum && prevRune != 0 {
-				newRunes = append(newRunes, prevRune)
+			if repeatNum, err := strconv.ParseInt(string(currRune), 10, 32); !prevRuneShielded && err == nil {
+				if prevRuneNum || prevRune == 0 {
+					if !prevRuneShielded {
+						return "", ErrInvalidString
+					}
+				}
+				prevRuneNum = true
+				for i := int64(0); i < repeatNum; i++ {
+					newRunes = append(newRunes, prevRune)
+				}
+			} else {
+				if prevRuneShielded && err != nil && currRune != '\\' {
+					return "", ErrInvalidString
+				}
+				if !prevRuneNum && prevRune != 0 {
+					newRunes = append(newRunes, prevRune)
+				}
+				if runeInd == len(runes)-1 {
+					newRunes = append(newRunes, currRune)
+				}
+				prevRuneNum = false
 			}
-			if runeInd == len(runes)-1 {
-				newRunes = append(newRunes, currRune)
-			}
-			prevRuneNum = false
+			prevRune = currRune
+			prevRuneShielded = false
 		}
-		prevRune = currRune
 	}
-
 	return string(newRunes), nil
 }
