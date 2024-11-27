239ebe261923843b8b5504a9c887416ec41d2eec
frobbery <femalerobbery68@gmail.com>
2024-11-27 15:27:10 +0300

    Fix lint

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,7 +8,6 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
-
 	runes := []rune(s)
 
 	newRunes := make([]rune, 0, len(runes))
@@ -25,16 +24,16 @@ func Unpack(s string) (string, error) {
 				newRunes = append(newRunes, prevRune)
 			}
 		} else {
-			if !prevRuneNum && prevRune != 0  {
+			if !prevRuneNum && prevRune != 0 {
 				newRunes = append(newRunes, prevRune)
 			}
-			if runeInd == len(runes) - 1 {
+			if runeInd == len(runes)-1 {
 				newRunes = append(newRunes, currRune)
 			}
 			prevRuneNum = false
 		}
 		prevRune = currRune
 	}
-	
+
 	return string(newRunes), nil
 }
