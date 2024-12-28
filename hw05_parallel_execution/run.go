5ad967b66d437c0498315b41edf39e2e048f3a05
frobbery <femalerobbery68@gmail.com>
2024-12-28 17:14:49 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,85 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type taskIndexLocked struct {
+	mu *sync.Mutex
+
+	taskIndex int
+
+	errorNumber int
+
+	err error
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	taskIndex := taskIndexLocked{
+		err: nil,
+
+		mu: &sync.Mutex{},
+
+		taskIndex: 0,
+
+		errorNumber: 0,
+	}
+
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+
+		go doTasks(tasks, m, &wg, &taskIndex)
+	}
+
+	wg.Wait()
+
+	return taskIndex.err
+}
+
+func doTasks(tasks []Task, maxErrorNumber int, wg *sync.WaitGroup, taskIndex *taskIndexLocked) {
+	defer wg.Done()
+
+	currTaskIndex := getNextTaskIndex(taskIndex)
+
+	for currTaskIndex < len(tasks) {
+		task := tasks[currTaskIndex]
+
+		err := task()
+
+		if maxErrorNumber > 0 && err != nil {
+			taskIndex.mu.Lock()
+
+			taskIndex.errorNumber++
+
+			if taskIndex.errorNumber >= maxErrorNumber {
+				taskIndex.taskIndex = len(tasks)
+
+				taskIndex.err = ErrErrorsLimitExceeded
+			}
+
+			taskIndex.mu.Unlock()
+		}
+
+		currTaskIndex = getNextTaskIndex(taskIndex)
+	}
+}
+
+func getNextTaskIndex(taskIndex *taskIndexLocked) int {
+	taskIndex.mu.Lock()
+
+	index := taskIndex.taskIndex
+
+	taskIndex.taskIndex++
+
+	taskIndex.mu.Unlock()
+
+	return index
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	//nolint:depguard
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 )
@@ -17,54 +18,101 @@ func TestRun(t *testing.T) {
 
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
+
 		tasks := make([]Task, 0, tasksCount)
 
 		var runTasksCount int32
 
 		for i := 0; i < tasksCount; i++ {
 			err := fmt.Errorf("error from task %d", i)
+
 			tasks = append(tasks, func() error {
 				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+
 				atomic.AddInt32(&runTasksCount, 1)
+
 				return err
 			})
 		}
 
 		workersCount := 10
+
 		maxErrorsCount := 23
+
 		err := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
 	t.Run("tasks without errors", func(t *testing.T) {
 		tasksCount := 50
+
 		tasks := make([]Task, 0, tasksCount)
 
 		var runTasksCount int32
+
 		var sumTime time.Duration
 
 		for i := 0; i < tasksCount; i++ {
 			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+
 			sumTime += taskSleep
 
 			tasks = append(tasks, func() error {
 				time.Sleep(taskSleep)
+
 				atomic.AddInt32(&runTasksCount, 1)
+
 				return nil
 			})
 		}
 
 		workersCount := 5
+
 		maxErrorsCount := 1
 
 		start := time.Now()
+
 		err := Run(tasks, workersCount, maxErrorsCount)
+
 		elapsedTime := time.Since(start)
+
 		require.NoError(t, err)
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("when m < 0, should ignore errors", func(t *testing.T) {
+		tasksCount := 50
+
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+
+				atomic.AddInt32(&runTasksCount, 1)
+
+				return err
+			})
+		}
+
+		workersCount := 10
+
+		maxErrorsCount := 0
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.NoError(t, err)
+
+		require.Equal(t, runTasksCount, int32(tasksCount))
 	})
 }
