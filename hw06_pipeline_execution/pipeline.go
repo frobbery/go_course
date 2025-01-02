d1cfcc1cf38af0e40bb0def1fb8580a63c783843
frobbery <femalerobbery68@gmail.com>
2025-01-02 22:59:12 +0300

    Fix done signal

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,7 +1,5 @@
 package hw06pipelineexecution
 
-import "sync"
-
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -11,50 +9,34 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	stagesChannels := make([]Bi, len(stages))
-	for i := 0; i < len(stages); i++ {
-		stagesChannels[i] = make(Bi)
-	}
-	subscribe(in, done, stages[0], stagesChannels[0])
+	currStageChannel := subscribe(in, done, stages[0])
 	for i := 1; i < len(stages); i++ {
-		subscribe(stagesChannels[i-1], done, stages[i], stagesChannels[i])
+		currStageChannel = subscribe(currStageChannel, done, stages[i])
 	}
-	readAllUnread(&stagesChannels)
-	return stagesChannels[len(stages)-1]
+	return currStageChannel
 }
 
-func subscribe(in In, done In, stage Stage, stageChannel Bi) {
+func subscribe(in In, done In, stage Stage) Out {
+	stageChannel := make(Bi)
 	go func() {
-		wg := sync.WaitGroup{}
-		outChannel := stage(in)
-		for i := range outChannel {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				select {
-				case <-done:
-					return
-				case stageChannel <- i:
-				}
-			}()
+		for i := range in {
+			select {
+			case <-done:
+				//nolint
+				defer readAllUnread(stageChannel)
+			default:
+				stageChannel <- i
+			}
 		}
-		go func() {
-			wg.Wait()
-			close(stageChannel)
-		}()
+		close(stageChannel)
 	}()
+	return stage(stageChannel)
 }
 
-func readAllUnread(stages *[]Bi) {
-	wg := sync.WaitGroup{}
-	for stage := range *stages {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for i := range stage {
-				_ = i
-			}
-		}()
-	}
-	wg.Wait()
+func readAllUnread(channel Bi) {
+	go func() {
+		for i := range channel {
+			_ = i
+		}
+	}()
 }
