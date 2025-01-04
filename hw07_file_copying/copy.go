68873cb96ebad5beafb0574fe78e15d33628e73c
frobbery <femalerobbery68@gmail.com>
2025-01-04 10:05:22 +0300

    HW07 completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -9,7 +11,56 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+type CopyBuffer struct {
+	buf *[]byte
+}
+
+func (cb *CopyBuffer) initBuf(fileSize, limit, offset int64) {
+	var buf []byte
+	if fileSize < limit-offset || limit == 0 {
+		buf = make([]byte, fileSize-offset)
+	} else {
+		buf = make([]byte, limit)
+	}
+	cb.buf = &buf
+}
+
+func (cb *CopyBuffer) readFrom(fromPath string, offset int64, limit int64) error {
+	file, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	if cb.buf == nil {
+		stats, err := file.Stat()
+		if err != nil {
+			return err
+		}
+		cb.initBuf(stats.Size(), limit, offset)
+	}
+	_, err = file.ReadAt(*cb.buf, offset)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
 	return nil
+}
+
+func (cb *CopyBuffer) writeTo(toPath string) error {
+	file, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(*cb.buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Copy(fromPath, toPath string, offset, limit int64) error {
+	cb := CopyBuffer{}
+	var err error
+	if err := cb.readFrom(fromPath, offset, limit); err == nil {
+		return cb.writeTo(toPath)
+	}
+	return err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,47 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	//nolint:depguard
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("Offset 0, limit 0", func(t *testing.T) {
+		err := Copy("testdata/input1.txt", "testdata/out1.txt", 0, 0)
+
+		require.Equal(t, nil, err, "Error occurred while copying")
+
+		tmpBuff, err := os.ReadFile("testdata/out1.txt")
+
+		require.Equal(t, nil, err, "Error occurred while reading created")
+
+		require.Equal(t, []byte("hello"), tmpBuff, "Written not expected")
+	})
+
+	t.Run("Offset 1, limit 2", func(t *testing.T) {
+		err := Copy("testdata/input1.txt", "testdata/out2.txt", 1, 2)
+
+		require.Equal(t, nil, err, "Error occurred while copying")
+
+		tmpBuff, err := os.ReadFile("testdata/out2.txt")
+
+		require.Equal(t, nil, err, "Error occurred while reading created")
+
+		require.Equal(t, []byte("el"), tmpBuff, "Written not expected")
+	})
+
+	t.Run("Offset 1, limit 10", func(t *testing.T) {
+		err := Copy("testdata/input1.txt", "testdata/out3.txt", 1, 10)
+
+		require.Equal(t, nil, err, "Error occurred while copying")
+
+		tmpBuff, err := os.ReadFile("testdata/out3.txt")
+
+		require.Equal(t, nil, err, "Error occurred while reading created")
+
+		require.Equal(t, []byte("ello"), tmpBuff, "Written not expected")
+	})
 }
