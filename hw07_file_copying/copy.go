59e6980c4216e4ad8a2ba120af26882429546d40
frobbery <femalerobbery68@gmail.com>
2025-01-08 20:09:28 +0300

    fix linter

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -8,9 +8,11 @@ import (
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
+	ErrUnsupportedFile = errors.New("unsupported file")
+
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
-	ErrSameInputOutput = errors.New("From path is equal to to path")
+
+	ErrSameInputOutput = errors.New("from path is equal to to path")
 )
 
 type CopyBuffer struct {
@@ -19,11 +21,13 @@ type CopyBuffer struct {
 
 func (cb *CopyBuffer) initBuf(fileSize, limit, offset int64) {
 	var buf []byte
+
 	if fileSize-offset < limit || limit == 0 {
 		buf = make([]byte, fileSize-offset)
 	} else {
 		buf = make([]byte, limit)
 	}
+
 	cb.buf = &buf
 }
 
@@ -32,18 +36,24 @@ func (cb *CopyBuffer) readFrom(fromPath string, offset int64, limit int64) error
 	if err != nil {
 		return err
 	}
+
 	defer file.Close()
+
 	if cb.buf == nil {
 		stats, err := file.Stat()
 		if err != nil {
 			return err
 		}
+
 		cb.initBuf(stats.Size(), limit, offset)
 	}
+
 	_, err = file.ReadAt(*cb.buf, offset)
+
 	if err != nil && !errors.Is(err, io.EOF) {
 		return err
 	}
+
 	return nil
 }
 
@@ -52,11 +62,14 @@ func (cb *CopyBuffer) writeTo(toPath string) error {
 	if err != nil {
 		return err
 	}
+
 	defer file.Close()
+
 	_, err = file.Write(*cb.buf)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
@@ -65,10 +78,13 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	if err != nil {
 		return err
 	}
+
 	cb := CopyBuffer{}
+
 	if err := cb.readFrom(fromPath, offset, limit); err == nil {
 		return cb.writeTo(toPath)
 	}
+
 	return err
 }
 
@@ -77,14 +93,17 @@ func validate(fromPath, toPath string, offset int64) error {
 	if err != nil {
 		return err
 	}
+
 	err = checkIfInputSupported(fromPath)
 	if err != nil {
 		return err
 	}
+
 	err = checkIfOffsetGreaterThanFileSize(fromPath, offset)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
@@ -93,13 +112,16 @@ func checkIfPathsEqual(fromPath, toPath string) error {
 	if err != nil {
 		return err
 	}
+
 	toAbsPath, err := filepath.Abs(toPath)
 	if err != nil {
 		return err
 	}
-	if (fromAbsPath == toAbsPath) {
-		return ErrSameInputOutput;
+
+	if fromAbsPath == toAbsPath {
+		return ErrSameInputOutput
 	}
+
 	return nil
 }
 
@@ -108,13 +130,16 @@ func checkIfInputSupported(fromPath string) error {
 	if err != nil {
 		return err
 	}
+
 	matchesProc, err := filepath.Match("*/proc*", fromPath)
 	if err != nil {
 		return err
 	}
+
 	if matchesUrandom || matchesProc {
 		return ErrUnsupportedFile
 	}
+
 	return nil
 }
 
@@ -123,11 +148,14 @@ func checkIfOffsetGreaterThanFileSize(fromPath string, offset int64) error {
 	if err != nil {
 		return err
 	}
+
 	if stats.IsDir() {
 		return ErrUnsupportedFile
 	}
+
 	if stats.Size() < offset {
 		return ErrOffsetExceedsFileSize
 	}
+
 	return nil
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -6,22 +6,29 @@ import (
 )
 
 var (
-	from, to      string
+	from, to string
+
 	limit, offset int64
 )
 
 func init() {
 	flag.StringVar(&from, "from", "", "file to read from")
+
 	flag.StringVar(&to, "to", "", "file to write to")
+
 	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
+
 	flag.Int64Var(&offset, "offset", 0, "offset in input file")
 }
 
 func main() {
 	flag.Parse()
+
 	if err := Copy(from, to, offset, limit); err != nil {
 		fmt.Printf("Error occurred while copying file, %v", err)
+
 		return
 	}
+
 	fmt.Printf("File %s successfully copied to file %s\n", from, to)
 }
