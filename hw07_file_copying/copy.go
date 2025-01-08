6488f00d4d9b3c10dd50d0e685db7e8b6bf8b33e
frobbery <femalerobbery68@gmail.com>
2025-01-08 20:03:32 +0300

    add validating input parameters

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -4,11 +4,13 @@ import (
 	"errors"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrSameInputOutput = errors.New("From path is equal to to path")
 )
 
 type CopyBuffer struct {
@@ -17,7 +19,6 @@ type CopyBuffer struct {
 
 func (cb *CopyBuffer) initBuf(fileSize, limit, offset int64) {
 	var buf []byte
-
 	if fileSize-offset < limit || limit == 0 {
 		buf = make([]byte, fileSize-offset)
 	} else {
@@ -60,10 +61,73 @@ func (cb *CopyBuffer) writeTo(toPath string) error {
 }
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
+	err := validate(fromPath, toPath, offset)
+	if err != nil {
+		return err
+	}
 	cb := CopyBuffer{}
-	var err error
 	if err := cb.readFrom(fromPath, offset, limit); err == nil {
 		return cb.writeTo(toPath)
 	}
 	return err
+}
+
+func validate(fromPath, toPath string, offset int64) error {
+	err := checkIfPathsEqual(fromPath, toPath)
+	if err != nil {
+		return err
+	}
+	err = checkIfInputSupported(fromPath)
+	if err != nil {
+		return err
+	}
+	err = checkIfOffsetGreaterThanFileSize(fromPath, offset)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkIfPathsEqual(fromPath, toPath string) error {
+	fromAbsPath, err := filepath.Abs(fromPath)
+	if err != nil {
+		return err
+	}
+	toAbsPath, err := filepath.Abs(toPath)
+	if err != nil {
+		return err
+	}
+	if (fromAbsPath == toAbsPath) {
+		return ErrSameInputOutput;
+	}
+	return nil
+}
+
+func checkIfInputSupported(fromPath string) error {
+	matchesUrandom, err := filepath.Match("*/dev/urandom/*", fromPath)
+	if err != nil {
+		return err
+	}
+	matchesProc, err := filepath.Match("*/proc*", fromPath)
+	if err != nil {
+		return err
+	}
+	if matchesUrandom || matchesProc {
+		return ErrUnsupportedFile
+	}
+	return nil
+}
+
+func checkIfOffsetGreaterThanFileSize(fromPath string, offset int64) error {
+	stats, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if stats.IsDir() {
+		return ErrUnsupportedFile
+	}
+	if stats.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+	return nil
 }
