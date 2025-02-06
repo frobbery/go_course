638ca0925f8af4473b2076637edee2e8b8d201bc
frobbery <femalerobbery68@gmail.com>
2025-02-06 12:08:53 +0300

    remove other files

--- hw12_13_14_15_calendar/internal/storage/event.go
+++ hw12_13_14_15_calendar/internal/storage/event.go
@@ -1,39 +1,7 @@
 package storage
 
-import (
-	"context"
-	"time"
-)
-
 type Event struct {
-	Id int64
-	Name string
-	DateTime time.Time
-	EndDateTime time.Time
-	Description string
-	UserId int64
-	SendBefore time.Time
-}
-
-type Notification struct {
-	EventId int64
-	Name string
-	EventDate time.Time
-	UserToSendId int64
-}
-
-type CalendarRepo interface {
-	CreateEvent(ctx context.Context, event Event) (eventId int64, err error)
-	UpdateEvent(ctx context.Context, eventId int64, event Event) (err error)
-	DeleteEvent(ctx context.Context, eventId int64) (err error)
-	EventsForDay(ctx context.Context, day time.Time) (events []Event, err error)
-	EventsForWeek(ctx context.Context, firstDayOfWeek time.Time) (events []Event, err error)
-	EventsForMonth(ctx context.Context, firstDayOfMonth time.Time) (events []Event,err error)
-}
-
-type Storage interface {
-	Connect(ctx context.Context, dsn string) error
-	Migrate(ctx context.Context, migrate string) error
-	Close() error
-	CalendarRepo
+	ID    string
+	Title string
+	// TODO
 }
--- hw12_13_14_15_calendar/internal/storage/memory/storage.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage.go
@@ -1,82 +1,14 @@
 package memorystorage
 
-import (
-	"sync"
-	"github.com/frobbery/go_course/hw12_13_14_15_calendar/internal/storage"
-	"context"
-	"time"
-)
+import "sync"
 
-type inMemoryStorage struct {
-	currentEventId 	int64
-	eventsById		map[int64]storage.Event
-	mu				sync.RWMutex
+type Storage struct {
+	// TODO
+	mu sync.RWMutex //nolint:unused
 }
 
-func New() storage.Storage {
-	return &inMemoryStorage{}
+func New() *Storage {
+	return &Storage{}
 }
 
-func (s *inMemoryStorage) Connect(ctx context.Context, dsn string) (err error) {
-	s.eventsById = make(map[int64]storage.Event)
-	return nil
-}
-
-func (s *inMemoryStorage) Migrate(ctx context.Context, migrate string) (err error) {
-	return nil
-}
-
-func (s *inMemoryStorage) Close() error {
-	for eventId := range s.eventsById {
-		delete(s.eventsById, eventId)
-	}
-	return nil
-}
-
-func (s *inMemoryStorage) CreateEvent(ctx context.Context, event storage.Event) (eventId int64, err error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.currentEventId++
-	event.Id = s.currentEventId
-	s.eventsById[s.currentEventId] = event
-	return s.currentEventId, nil
-}
-
-func (s *inMemoryStorage) UpdateEvent(ctx context.Context, eventId int64, event storage.Event) (err error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.eventsById[eventId] = event
-	return nil
-}
-
-func (s *inMemoryStorage) DeleteEvent(ctx context.Context, eventId int64) (err error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	delete(s.eventsById, eventId)
-	return nil
-}
-
-func(s *inMemoryStorage) getEventsBetweenTwoDates(_ context.Context, startDate time.Time, endDate time.Time) (events []storage.Event, err error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	events = make([]storage.Event, 0)
-	for eventId := range s.eventsById {
-		curDateTime := s.eventsById[eventId].DateTime
-		if !curDateTime.Before(startDate) && !curDateTime.After(endDate) {
-			events = append(events, s.eventsById[eventId])
-		}
-	}
-	return events, nil
-}
-
-func (s *inMemoryStorage) EventsForDay(ctx context.Context, day time.Time) (events []storage.Event, err error) {
-	return s.getEventsBetweenTwoDates(ctx, day, day.Add(time.Duration(time.Hour * 24)));
-}
-
-func (s *inMemoryStorage) EventsForWeek(ctx context.Context, day time.Time) (events []storage.Event, err error) {
-	return s.getEventsBetweenTwoDates(ctx, day, day.Add(time.Duration(time.Hour * 24 * 7)));
-}
-
-func (s *inMemoryStorage) EventsForMonth(ctx context.Context, day time.Time) (events []storage.Event, err error) {
-	return s.getEventsBetweenTwoDates(ctx, day, day.Add(time.Duration(time.Hour * 24 * 30)));
-}
+// TODO
